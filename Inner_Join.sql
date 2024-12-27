4e4d7943f4ccfead93a75df80251a7a6a1eb8430
Samyak Shrestha <samyak.dx@gmail.com>
2024-12-27 00:56:58 -0600

    Add files via upload

--- Inner_Join.sql
+++ Inner_Join.sql
@@ -1,7 +1,18 @@
-SELECT first_name, last_name, title
-FROM film_actor
-INNER JOIN actor
-ON film_actor.actor_id = actor.actor_id
-INNER JOIN film 
-ON film.film_id = film_actor.film_id
-WHERE first_name = 'Nick' AND last_name = 'Wahlberg'
+-- Query to retrieve the first name, last name, and film title
+-- of films featuring the actor 'Nick Wahlberg'
+-- This task requires joing three tables
+SELECT 
+    actor.first_name, 
+    actor.last_name, 
+    film.title
+FROM 
+    film_actor
+INNER JOIN 
+    actor
+    ON film_actor.actor_id = actor.actor_id
+INNER JOIN 
+    film
+    ON film.film_id = film_actor.film_id
+WHERE 
+    actor.first_name = 'Nick' 
+    AND actor.last_name = 'Wahlberg';
